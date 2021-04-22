7b5545bdaa6d10638d03b6e56d13cd114e78106f
robert.zhang <yz3919@nyu.edu>
2021-04-22 22:19:56 +0800

    Fixed avatar_id bug

--- static/js/login.js
+++ static/js/login.js
@@ -1,40 +1,15 @@
 
 
 $(window).load(function(){
-    var avatar_id = 1;
 
-    $("#next-btn").click(function(){
-        if (avatar_id==15){
-            avatar_id = 1;
-        }
-        else{
-            avatar_id += 1;
-        }
-    })
-    
-    
-    $("#prev-btn").click(function(){
-        if (avatar_id==1){
-            avatar_id = 15;
-        }
-        else{
-            avatar_id -= 1;
-        }
-    })
-    
     $("#login-btn").click(function(e){
-        var result = "";
-        if (avatar_id<10){
-            result = "0"+avatar_id.toString();
-        }
-        result = "avatar"+result;
         e.preventDefault();
         $.ajax({
             url:"/login",
             type:"POST",
             data:{
                 username:$("#username").val(),
-                avatar_id:result
+                avatar_id:"avatar"+$(".carousel-item.active").attr("id")
             },
             success: function(data){
                 if (!data.status){
